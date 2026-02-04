84b7ae2cfdafea33d8b7dd8cc651c8118cc852c6
NanditaReji-STUDENT <g00463584@atu.ie>
2026-02-04 17:47:27 +0000

    Created project for OOP

---
+++ src/main/java/com/example/oop_hallbooking/OopHallBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.oop_hallbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopHallBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopHallBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oop_hallbooking/OopHallBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oop_hallbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopHallBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
