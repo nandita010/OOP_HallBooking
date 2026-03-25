2c4a14af8e5f388f5f3819dbac472efcd121a5c6
deira <deiravalci@gmail.com>
2026-03-25 14:43:19 +0000

    Login Works.

--- src/main/java/com/example/oop_hallbooking/Service/LoginService.java
+++ src/main/java/com/example/oop_hallbooking/Service/LoginService.java
@@ -1,15 +1,31 @@
 package com.example.oop_hallbooking.Service;
 
+import com.example.oop_hallbooking.Model.Login;
+import com.example.oop_hallbooking.Repository.LoginRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LoginService {
-    private String name;
-    private String email;
-    private String dob;
-    private String password;
 
+    private List<Login> logins;
+    private final LoginRepo loginRepo;
 
+    public LoginService(LoginRepo loginRepo){
+        this.loginRepo = loginRepo;
+    }
 
+    public Login addLogins(Login login){
+        logins = loginRepo.findAll();
+
+        loginRepo.save(login);
+        return login;
+    }
+
+    //Get All
+    public List<Login> getAllLogins(){
+        return loginRepo.findAll();
+    }
 
 }
